7da80f99a4e0225e6f9498faf960eba8145a27dc
José Anthony Arenas Conde <142860250+JoseACd744@users.noreply.github.com>
2023-11-05 15:29:32 -0500

    Feature: Device Controller

---
+++ PhoneResQ.API/Support/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhoneResQ.API.Support.Controllers;
+
+[Route(template:"api/v1/[controller]")]
+[ApiController]
+
+public class OrderController
+{
+    
+}
