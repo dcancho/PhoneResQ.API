bcfbd86fa5eab33d5d2987b74e87dd312c0f5b10
José Anthony Arenas Conde <142860250+JoseACd744@users.noreply.github.com>
2023-11-05 16:01:00 -0500

    Feature: Technician Controller

---
+++ PhoneResQ.API/Support/Controllers/TechnicianController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneResQ.API.Shared.Infrastructure.Configuration.Extensions;
+using PhoneResQ.API.Support.Domain.Models.Entities;
+using PhoneResQ.API.Support.Domain.Services;
+using PhoneResQ.API.Support.Resources;
+
+namespace PhoneResQ.API.Support.Controllers;
+
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class TechnicianController: ControllerBase
+{
+    private readonly ITechnicianService _technicianService;
+
+    public TechnicianController(ITechnicianService technicianService)
+    {
+        _technicianService = technicianService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var technicians = await _technicianService.ListAsync();
+        var technicianResources = MapTechniciansToTechnicianResources(technicians);
+        return Ok(technicianResources);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] SaveTechnicianResource resource)
+    {
+        // Validation of the resource
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState.GetErrorMessages());
+        }
+        var technician = new Technician
+        {
+            Name = resource.Name,
+            DNI = resource.DNI,
+            Address = resource.Address,
+            Password = resource.Password,
+        };
+        // Saving the technician (interaction with service)
+        var result = await _technicianService.SaveAsync(technician);  // If the result is not successful, return the error message
+        if (!result.Success)
+        {
+            return BadRequest(result.Message);
+        }
+        // Returning the action result
+        return Ok(result);
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync(int id, [FromBody] SaveTechnicianResource resource)
+    {
+        // Validation of the resource
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState.GetErrorMessages());
+        }
+        var technician = new Technician
+        {
+            Name = resource.Name,
+            DNI = resource.DNI,
+            Address = resource.Address,
+            Password = resource.Password,
+        };
+        // Saving the technician (interaction with service)
+        var result = await _technicianService.UpdateAsync(id, technician);  // If the result is not successful, return the error message
+        if (!result.Success)
+        {
+            return BadRequest(result.Message);
+        }
+        // Returning the action result
+        return Ok(result);
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var result = await _technicianService.DeleteAsync(id);
+        if (!result.Success)
+        {
+            return BadRequest(result.Message);
+        }
+        return Ok(result);
+    }
+    
+    private static List<TechnicianResource> MapTechniciansToTechnicianResources(IEnumerable<Technician> technicians)
+    {
+        return technicians.Select(technician => new TechnicianResource
+        {
+            Id = technician.Id,
+            Name = technician.Name,
+            DNI = technician.DNI,
+            Address = technician.Address,
+            Password = technician.Password
+        }).ToList();
+    }
+}
